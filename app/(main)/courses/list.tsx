58689a11dcb736baaa9eb70edb3ffe8b556d3361
HoangKyPham <hoangkypham1682@gmail.com>
2024-05-16 01:20:17 +0700

    update

--- app/(main)/courses/list.tsx
+++ app/(main)/courses/list.tsx
@@ -9,42 +9,42 @@ import { toast } from 'sonner';
 
 type Props = {
     courses: typeof courses.$inferSelect[];
-    activeCourseId ?: typeof userProgress.$inferSelect.activeCourseId;
+    activeCourseId?: typeof userProgress.$inferSelect.activeCourseId;
 }
 
-const List = ({courses, activeCourseId}: Props) => {
+const List = ({ courses, activeCourseId }: Props) => {
     const router = useRouter();
     const [pending, startTransition] = useTransition();
 
-    const onClick = (id : number) => {
-        if(pending) return;
+    const onClick = (id: number) => {
+        if (pending) return;
 
-        if(id === activeCourseId) {
+        if (id === activeCourseId) {
             return router.push("/learn")
         }
 
         startTransition(() => {
             upsertUserProgress(id)
-            .catch(() => toast.error("Something went wrong"))
+                .catch(() => toast.error("Something went wrong"))
         })
     }
 
 
-  return (
-    <div className="pt-6 grid grid-cols-2 lg:grid-cols-[repeat(auto-fill, minmax(210px,1fr))] gap-4">
-        {courses.map(course => (
-            <Card 
-                key={course.id}
-                id={course.id}
-                title={course.title}
-                imageSrc={course.imageSrc}
-                onClick={onClick}
-                disabled = {pending}
-                active={course.id === activeCourseId}
-            />
-        ))}
-    </div>
-  )
+    return (
+        <div className="pt-6 grid grid-cols-2 lg:grid-cols-[repeat(auto-fill, minmax(210px,1fr))] gap-4">
+            {courses.map(course => (
+                <Card
+                    key={course.id}
+                    id={course.id}
+                    title={course.title}
+                    imageSrc={course.imageSrc}
+                    onClick={onClick}
+                    disabled={pending}
+                    active={course.id === activeCourseId}
+                />
+            ))}
+        </div>
+    )
 }
 
 export default List
--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,28 +1,65 @@
 import Header from '@/app/(main)/learn/header'
+import Unit from '@/app/(main)/learn/unit'
 import FeedWrapper from '@/components/feed-wrapper'
 import StickyWrapper from '@/components/sticky-wrapper'
 import UserProgess from '@/components/user-progress'
-import { getUserProgress } from '@/db/queries'
+import { getCourseProgress, getLessonPercentage, getUnits, getUserProgress } from '@/db/queries'
+import { lessons, units as unitsSchema } from '@/db/schema'
 import { redirect } from 'next/navigation'
 import React from 'react'
 
 const LearnPage = async () => {
   const userProgressData = getUserProgress();
+  const unitsData: any = getUnits()
+  const courseProgressData: any = getCourseProgress()
+  const lessonPercentageData: any = getLessonPercentage()
 
-  const [userProgress] = await Promise.all([userProgressData]);
 
-  if(!userProgress || !userProgress.activeCourse){
+  const [
+    userProgress,
+    units,
+    courseProgress,
+    lessonPercentage
+  ] = await Promise.all([
+    userProgressData,
+    unitsData,
+    courseProgressData,
+    lessonPercentageData
+  ]);
+
+  if (!userProgress || !userProgress.activeCourse) {
     redirect("/courses")
+  }
+
+  if(!courseProgress){
+    redirect("/course")
   }
 
   return (
     <div className='flex flex-row-reverse gap-[48px] px-6'>
-        <StickyWrapper>
-            <UserProgess activeCourse={userProgress.activeCourse} hearts={userProgress.hearts} points={userProgress.points} hasActiveSubscription={false} />
-        </StickyWrapper>
-        <FeedWrapper>
-            <Header title={userProgress.activeCourse.title} />
-        </FeedWrapper>
+      <StickyWrapper>
+        <UserProgess activeCourse={userProgress.activeCourse} hearts={userProgress.hearts} points={userProgress.points} hasActiveSubscription={false} />
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title={userProgress.activeCourse.title} />
+        {units.map((unit: any) => (
+          <div key={unit.id} className="mb-10">
+            <Unit
+              id={unit.id}
+              order={unit.order}
+              description={unit.description}
+              title={unit.title}
+              lessons={unit.lessons}
+              activeLesson={
+                courseProgress.activeLesson as typeof lessons.$inferSelect & {
+                  unit : typeof unitsSchema.$inferSelect;
+                } | undefined
+              }
+              activeLessonPercentage={lessonPercentage}
+            />
+          </div>
+        ))}
+      </FeedWrapper>
     </div>
   )
 }
