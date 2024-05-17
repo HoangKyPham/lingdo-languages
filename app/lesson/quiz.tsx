332ea02852a858471575baaf71622febbe5ca2b6
HoangKyPham <hoangkypham1682@gmail.com>
2024-05-17 17:01:05 +0700

    update

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -1,10 +1,19 @@
 "use client"
 
+import { upsertChallengeProgress } from '@/actions/challenge-progress';
+import { reduceHearts } from '@/actions/user-progress';
 import Challenge from '@/app/lesson/challenge';
+import Confetti from "react-confetti"
+import Footer from '@/app/lesson/footer';
 import Header from '@/app/lesson/header';
 import QuestionBubble from '@/app/lesson/question-bubble';
+import ResultCard from '@/app/lesson/result-card';
 import { challengeOptions, challenges } from '@/db/schema';
-import React, { useState } from 'react'
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import React, { useState, useTransition } from 'react'
+import { useAudio, useWindowSize } from 'react-use';
+import { toast } from 'sonner';
 
 type Props = {
     initialPercentage: number;
@@ -24,6 +33,23 @@ const Quiz = ({
     initialLessonId,
     userSubscription
 }: Props) => {
+    const {width, height} = useWindowSize();
+    const router = useRouter();
+
+    const [finishAudio] = useAudio({src: "/finish.mp3", autoPlay: true});    
+    const [
+        correctAudio,
+        _c,
+        correctControls
+    ] = useAudio({src: "/correct.wav"});
+    const [
+        incorrectAudio,
+        _i,
+        incorrectControls
+    ] = useAudio({src: "/incorrect.wav"});
+
+    const [pending, startTransition] = useTransition();
+    const [lessonId] = useState(initialLessonId)
     const [hearts, setHearts] = useState(initialHearts);
     const [percentage, setPercentage] = useState(initialPercentage);
     const [challenges] = useState(initialLessonChallenges);
@@ -32,14 +58,137 @@ const Quiz = ({
         return uncompletedIndex === -1 ? 0 : uncompletedIndex;
     })
 
+    const [selectedOption, setSelectedOption] = useState<number>()
+    const [status, setStatus] = useState<"correct" | "wrong" | "none">("none")
+
     const challenge = challenges[activeIndex];
-    const options = challenge?.challengeOptions ?? []
+    const options = challenge?.challengeOptions ?? [];
+
+    const onNext = () => {
+        setActiveIndex((current) => current + 1)
+    }
+
+    const onSelect = (id: number) => {
+        if (status !== "none") return
+        setSelectedOption(id)
+    }
+
+    const onContinue = () => {
+        if (!selectedOption) return
+
+        if (status === "wrong") {
+            setStatus("none");
+            setSelectedOption(undefined);
+            return;
+        }
+        if (status === "correct") {
+            onNext();
+            setStatus("none");
+            setSelectedOption(undefined);
+            return;
+        }
+
+        const correctOption = options.find((option) => option.correct)
+
+        if (!correctOption) {
+            return
+        }
+
+        if (correctOption.id === selectedOption) {
+            startTransition(() => {
+                upsertChallengeProgress(challenge.id)
+                    .then((response) => {
+                        if (response?.error === "hearts") {
+                            console.error("Missing hearts");
+                            return
+                        }
+
+                        correctControls.play()
+                        setStatus("correct");
+                        setPercentage(prev => prev + 100 / challenges.length);
+
+                        if (initialPercentage === 100) {
+                            setHearts(prev => Math.min(prev + 1, 5))
+                        }
+                    })
+                    .catch(() => toast.error("Something went wrong. Please try again"))
+            })
+        } else {
+            startTransition(() => {
+                reduceHearts(challenge.id)
+                    .then((respone) => {
+                        if (respone?.error === "hearts") {
+                            console.error("missing hearts");
+                            return;
+                        }
+
+                        incorrectControls.play()
+                        setStatus("wrong");
+
+                        if (!respone?.error) {
+                            setHearts((prev) => Math.max(prev - 1, 0))
+                        }
+                    })
+                    .catch (() => toast.error("Something went wrong. Please try again"))
+            })
+        }
+    }
+
+    if(!challenge){
+        return (
+            <>
+                {finishAudio}
+                <Confetti 
+                    width={width}
+                    height={height}
+                    recycle={false}
+                    numberOfPieces={500}
+                    tweenDuration={10000}
+                />
+                <div className=" flex flex-col gap-y-4 lg:gap-y-8 max-w-lg mx-auto text-center items-center justify-center h-full">
+                    <Image
+                        src="/finish.svg"
+                        alt='finish'
+                        className='hidden lg:block'
+                        height={100}
+                        width={100}
+                    />
+                    <Image
+                        src="/finish.svg"
+                        alt='finish'
+                        className='block lg:hidden'
+                        height={50}
+                        width={50}
+                    />
+                    <h1 className='text-xl lg:text-3xl font-bold text-neutral-700'>
+                        Great job <br /> You've complted the lesson.
+                    </h1>
+                    <div className="flex items-center gap-x-4">
+                        <ResultCard 
+                            variant="points"
+                            value={challenges.length * 10}
+                        />
+                        <ResultCard 
+                            variant="hearts"
+                            value={hearts}
+                        />
+                    </div>
+                </div>
+                <Footer
+                    lessonId={lessonId}
+                    status='completed'
+                    onCheck={() => router.push("/learn")}
+                />
+            </>
+        )
+    }    
 
     const title = challenge.type === "ASSIST" ? "Select the correct meaning" : challenge.question
 
-
     return (
         <>
+            {correctAudio}
+            {incorrectAudio}
             <Header
                 hearts={hearts}
                 percentage={percentage}
@@ -57,16 +206,21 @@ const Quiz = ({
                             )}
                             <Challenge
                                 options={options}
-                                onSelect={() => {}}
-                                status="none"
-                                selectedOption={undefined}
-                                disabled={false}
+                                onSelect={onSelect}
+                                status={status}
+                                selectedOption={selectedOption}
+                                disabled={pending}
                                 type={challenge.type}
                             />
                         </div>
                     </div>
                 </div>
             </div>
+            <Footer
+                disabled={pending || !selectedOption}
+                status={status}
+                onCheck={onContinue}
+            />
         </>
     )
 }
---
+++ app/lesson/result-card.tsx
@@ -0,0 +1,49 @@
+import { cn } from '@/lib/utils';
+import Image from 'next/image';
+import React from 'react'
+
+type Props = {
+    value: number;
+    variant: "points" | "hearts";
+}
+
+const ResultCard = ({ value, variant }: Props) => {
+    const imageSrc = variant === "hearts" ? "/heart.svg" : "/points.svg"
+
+    return (
+        <div
+            className={cn(
+                "rounded-2xl border-2 w-full",
+                variant === "points" && "bg-orange-400 border-orange-400",
+                variant === "hearts" && "bg-rose-400 border-rose-400"
+            )}
+        >
+            
+            <div className={cn(
+                "p-1.5 text-white rounded-t-xl font-bold text-center uppercase text-xs",
+                variant === "hearts" && " border-rose-400",
+                variant === "points" && " border-orange-400",
+
+            )}>
+                {variant === "hearts" ? "Hearts Left" : "Total XP"}
+            </div>
+            <div className={cn(
+                "rounded-2xl bg-white items-center flex justify-center p-6 font-bold text-lg",
+                variant === "hearts" && " text-rose-400",
+                variant === "points" && " text-orange-400",
+            )}>
+                <Image
+                    alt='Icon'
+                    src={imageSrc}
+                    height={30}
+                    width={30}
+                    className='mt-1.5'
+                />
+                {value}
+            </div>
+
+        </div>
+    )
+}
+
+export default ResultCard
